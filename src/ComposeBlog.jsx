98610678cff275544e135665c9276821f8858a1e
localhostwiththemost <jpowelldev@gmail.com>
2023-05-06 12:04:23 -0500

    Styles for Compose Blog page

--- src/ComposeBlog.jsx
+++ src/ComposeBlog.jsx
@@ -1,10 +1,44 @@
+import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar";
+import { auth } from "./firebase";
 
 function ComposeBlog({ loggedIn }) {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  function getFirstName(displayName) {
+    if (!displayName) {
+      return '';
+    }
+    const firstName = displayName.split(' ')[0];
+    return firstName;
+}
+
   return (
     <>
       <section className="compose-page">
         <Navbar loggedIn={loggedIn} />
+
+        {user ? (
+          <div>
+            <h1 className="welcome-message">Welcome, {getFirstName(user.displayName)}!</h1>
+          </div>
+        ) : (
+          ""
+        )}
 
         <form action="" className="compose-form">
           <label htmlFor="blog-title" className="compose-form__label">
@@ -27,6 +61,8 @@ function ComposeBlog({ loggedIn }) {
             rows="20"
             placeholder="Offer your unique perspective on a subject"
           ></textarea>
+          
+          <input type="file" className="img-input" />
           <button className="compose-form__btn">Post</button>
         </form>
       </section>
