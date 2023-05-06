55579d1867ba46f854d53ecd050a3e3a1e6206be
localhostwiththemost <jpowelldev@gmail.com>
2023-05-06 17:12:18 -0500

    Form now submits blog data to Firebase db on submit

--- src/ComposeBlog.jsx
+++ src/ComposeBlog.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar";
-import { auth } from "./firebase";
+import { auth, db } from "./firebase";
+import { addBlog } from "./blogCollection";
 
 function ComposeBlog({ loggedIn }) {
   const [user, setUser] = useState(null);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -21,11 +24,31 @@ function ComposeBlog({ loggedIn }) {
 
   function getFirstName(displayName) {
     if (!displayName) {
-      return '';
+      return "";
     }
-    const firstName = displayName.split(' ')[0];
+    const firstName = displayName.split(" ")[0];
     return firstName;
-}
+  }
+
+  // Update the title state variable when the input value changes
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  // Update the content state variable when the textarea value changes
+  const handleContentChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  // todo
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let blogData = { user: { displayName: user.displayName }, title, content };
+    addBlog(db, blogData);
+
+    setTitle("");
+    setContent("");
+  };
 
   return (
     <>
@@ -34,13 +57,15 @@ function ComposeBlog({ loggedIn }) {
 
         {user ? (
           <div>
-            <h1 className="welcome-message">Welcome, {getFirstName(user.displayName)}!</h1>
+            <h1 className="welcome-message">
+              Welcome, {getFirstName(user.displayName)}!
+            </h1>
           </div>
         ) : (
           ""
         )}
 
-        <form action="" className="compose-form">
+        <form className="compose-form" onSubmit={handleSubmit}>
           <label htmlFor="blog-title" className="compose-form__label">
             Title
           </label>
@@ -49,6 +74,8 @@ function ComposeBlog({ loggedIn }) {
             className="blog-title"
             id="blog-title"
             placeholder="Summarize your blog post"
+            value={title}
+            onChange={handleTitleChange}
           />
           <label htmlFor="blog-text" className="compose-form__label">
             Compose your article
@@ -60,8 +87,10 @@ function ComposeBlog({ loggedIn }) {
             cols="80"
             rows="20"
             placeholder="Offer your unique perspective on a subject"
+            value={content}
+            onChange={handleContentChange}
           ></textarea>
-          
+
           <input type="file" className="img-input" />
           <button className="compose-form__btn">Post</button>
         </form>
